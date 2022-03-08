cf25c1809a407e889c9a292b801c2c765e03da39
Cole Dylewski <cole.dylewski@gmail.com>
2022-03-08 13:38:14 -0500

    3/8/2022 - Updating time comparisons. timeframe now uses relative differences ala M.-A. Lemburg

--- extract.py
+++ extract.py
@@ -12,6 +12,7 @@ import datetime as dt
 #non-standard libraries
 import pandas_market_calendars as mcal
 from tzlocal import get_localzone
+from dateutil.relativedelta import relativedelta
 
 #internal libraries
 import coreFuncs
@@ -19,27 +20,35 @@ import coreFuncs
 def getClock():
     print('')
     localTZOffset = time.timezone / 3600.0
-    localTZ = pd.Timestamp(dt.datetime.now().astimezone()).tzinfo
+    #get local time and timezone
     nowTS = pd.Timestamp(dt.datetime.now().astimezone())
+    localTZ = nowTS.tzinfo
+
+    #convert to UTC for standard comparison
     nowUTC = pd.Timestamp(dt.datetime.now().astimezone()).tz_convert('UTC')
 
-    #nowTS = pd.Timestamp(dt.datetime.now().astimezone()).tz_convert(localTZ)
-
     nyse = mcal.get_calendar('NYSE')
-    lastWeek = nyse.valid_days(
-        start_date=nowUTC.date() - dt.timedelta(days=7),
+    lastMonth = nyse.valid_days(
+        start_date=nowUTC.date() - relativedelta(months=1),
         end_date=nowUTC.date())
-    lastDate = nyse.valid_days(
-        start_date=nowUTC.date() - dt.timedelta(days=7),
-        end_date=nowUTC.date())[-1]
+    lastWeek = nyse.valid_days(
+        start_date=nowUTC.date() - relativedelta(weeks=1),
+        end_date=nowUTC.date())
+    lastYear = nyse.valid_days(
+        start_date=nowUTC.date() - relativedelta(years=1),
+        end_date=nowUTC.date())
+    lastDate = lastWeek[-1]
 
+    print('last year',lastYear)
+    print('last month', lastMonth)
     print('last week', lastWeek)
     print('last date', lastDate)
-    weekSchedule = nyse.schedule(start_date=lastWeek[0].date(), end_date=nowUTC.date())
+
+
     todaysSchedule = nyse.schedule(start_date=nowUTC.date(), end_date=nowUTC.date())
     lastOpen = pd.Timestamp(todaysSchedule['market_open'][0])
     lastClose = pd.Timestamp(todaysSchedule['market_close'][0])
-    print(weekSchedule)
+
     print(todaysSchedule)
     print('Last Open',lastOpen.tz_convert(localTZ))
     #print(dt.datetime.fromtimestamp(lastOpen))
